72ce9d2d384e3bfa355cda7986415f3429f6f861
vinceh121 <vincent.hyvert@sfr.fr>
2020-09-23 09:48:00 +0200

    It works! and it's recursive

--- create_web.pl
+++ create_web.pl
@@ -26,57 +26,74 @@ use warnings;
 
 use Path::Tiny;
 use Term::ANSIColor;
+use Getopt::Std;
+
+our($opt_l);
+
+$opt_l = "libreoffice";
+
+exit 0 if !getopts("l:");
 
 print colored("Generating web pages...", "magenta"), "\n";
 
 my $start = path(".");
 my $out = path("./html/");
+$out->mkpath();
 
-my $subs = $start->iterator;
+my $files = $start->iterator( {recurse => 1} );
 
-while (my $sub = $subs->()) { # Go over every folder
-	next if $sub->is_file();
-	next if $sub->basename eq ".git";
-	print colored("Subject $sub", "yellow"), "\n";
 
-	my $files = $sub->iterator;
+while (my $f = $files->()) { # Go over every file in folder
+	next if !($f =~ m/.fodt/ || $f =~ m/.fodg/ || $f =~ m/.fods/) && ($f =~ m/^\.git/ || $f =~ m/html/);
+	my $outfmt = "html";
 
-	while (my $f = $files->()) { # Go over every file in folder
-		next if !($f =~ m/.fodt/ || $f =~ m/.fodg/ || $f =~ m/.fods/);
-		my $outfmt = "html";
-
-		if ($f =~ m/.fodg/) {
-			$outfmt = "svg";
-		}
-
-		my $outsub = path($out, $sub->basename);
-		my $nf = path($outsub, $f->basename(".fodt") . "." . $outfmt);
-		print "\t", colored("$f -> $nf", "green"), "\n";
-		system("libreoffice --convert-to " . $outfmt . " --outdir \"" . $outsub . "\" \"" . $f . "\"");
+	if ($f =~ m/.fodg/) {
+		$outfmt = "svg";
 	}
+
+	my $outsub = path($out, $f->parent);
+	my $nf = path($outsub, $f . "." . $outfmt);
+	print "\t", colored("$f -> $nf", "cyan"), "\n";
+	system($opt_l . " --convert-to " . $outfmt . " --outdir \"" . $outsub . "\" \"" . $f . "\"");
 }
+
 
 print colored("Generating index...", "magenta"), "\n";
 
 my $index = "<html><head><link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'></head><body class='w3-indigo w3-padding'><h1>Index of files</h1><ul style='list-style: none'>\n";
 
-$subs = $out->iterator;
+#my $subs = $out->iterator( {recurse => 1} );
 
-while (my $sub = $subs->()) {
-	next if $sub->is_file();
-	my $files = $sub->iterator;
+#while (my $f = $subs->()) {
+#	next if !(($f =~ /html$/) || ($f =~ /svg$/));
+#	print "\t", colored("Adding $f", "green"), "\n";
+#	my $url = substr($f, 5, length($f) - 5);
+#	$index = $index . "\t\t<li class='w3-button'><a href=\"" . $url . "\">" . $f->basename . "</a></li>\n";
+#}
 
-	$index = $index . "\t<div class='w3-card w3-blue w3-margin w3-padding'><li>" . $sub->basename . "</li><ul>\n";
-
-	while (my $f = $files->()) {
-		next if !(($f =~ /html$/) || ($f =~ /svg$/));
-		print "\t", colored("Adding $f", "green"), "\n";
-		my $url = substr($f, 5, length($f) - 5);
-		$index = $index . "\t\t<li class='w3-button'><a href=\"" . $url . "\">" . $f->basename . "</a></li>\n";
+sub AddFolder {
+	my $file = @_[0];
+	if ( $file->is_dir ) {
+		print "\t", colored("Adding $file", "green"), "\n";
+		$index = $index . "\t<li>" .$file->basename . "</li>";
+		$index = $index . "\t<ul>";
+		my $iter = $file->iterator;
+		while ( my $sub = $iter->() ) {
+			AddFolder($sub);
+		}
+		$index = $index . "\t</ul>";
+	} else {
+		return if !($file =~ m/\.html/);
+		print "\t", colored("Adding $file", "blue"), "\n";
+		my $url = substr($file, 5, length($file) - 5);
+		$index = $index . "\t\t<li class='w3-button'><a href=\"$url\">" . $file->basename . "</a></li>\n";
 	}
-
-	$index = $index . "\t</ul></div>\n";
 }
+
+AddFolder($out);
+
+$index = $index . "\t</ul></div>\n";
+
 
 $index = $index . "</ul></body></html>";
 
